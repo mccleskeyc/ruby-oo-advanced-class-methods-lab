ea072528f0f00bcada4082af3e2ef48ba85b7ced
mccleskeyc <mccleskeyc@gmail.com>
2020-11-19 22:15:06 -0500

    commit tree 7f9960ed518f53d537b7f78d70a04eec37ec821e

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title)
+    search = self.all.detect{|song| song.name == title}
+    search
+  end
+
+  def self.find_or_create_by_name(title)
+    search = self.find_by_name(title)
+    if search
+      search
+    else
+      self.create_by_name(title)
+    end
+  end
+
+  def self.alphabetical
+    alphabatized = self.all.sort_by {|song| song.name}
+    alphabatized
+  end
+
+  def self.new_from_filename(file)
+    parts = file.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(file)
+    parts = file.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
